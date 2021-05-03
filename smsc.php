861bd32e297c526fa77a18777f0a4e430aabca96
Dmitri Agababaev <agdsign@gmail.com>
2021-05-03 14:40:52 +0300

    Update smsc.php

--- smsc.php
+++ smsc.php
@@ -11,5 +11,22 @@ $sms_handle = new SMS_data_handle($PDO);
 $sms_handle->save();
 // $sms_handle->send();
 
+function XSS_secure() {
+
+	function replace($arr) {
+		$filter = array("<", ">");
+		$filter_replace = array("&lt;", "&gt;");
+
+		for ($i=0; $i < count($filter) ; $i++) {
+			$str = str_replace($filter[$i], $filter_replace[$i], $arr);
+		}
+		return $str;
+	}
+
+	if ($_GET) $_GET = replace($_GET);
+	if ($_POST) $_POST = replace($_POST);
+
+}
+
 
 ?>
